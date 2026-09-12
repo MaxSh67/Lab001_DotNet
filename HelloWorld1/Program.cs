766854a9a4eda7f7b9e29fab82117ade809726af
Max67 <maxhann0907@gmail.com>
2026-09-12 11:38:04 +0300

    test

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -1,4 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет от ИСП-242!");
-Console.WriteLine("Автор: Шаранов Максим, Изингер Алексей");
-Console.WriteLine("Год: 2026");
+int age = 20;
+double gpa = 4.5;
+bool isStudent = true;
+string name = "Максим";
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(gpa);
+Console.WriteLine(isStudent);
+
+var city = "Москва";
+var year = 2026;
+var pi = 3.14159;
+var isAcive = true;
+
+string myName = "Максим";
+int myAge = 18;
+string myGroup = "ИСП-242";
+Console.WriteLine($"Меня зовут {myName}, мне {myAge}, лет, я учусь в группе {myGroup}.")
+
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Привет {name2}!")
+
+Console.Write("Введите ваш возраст: ");
+string input = Console.ReadLine();
+int age2 = int.Parse(input);
+Console.WriteLine($"Через 10 лет вам будет {age2 + 10} лет.")
+
+// int x = 10;
+// int y = 3;
+// Console.WriteLine(x + y);
+// Console.WriteLine(x - y);
+// Console.WriteLine(x * y);
+// Console.WriteLine(x / y);
+// Console.WriteLine(x % y);
+
+string firstName = "Максим";
+string lastName = "Шаранов";
+string group = "ИСП-242";
+int birthYear = 2007;
+double gpa = 4.5;
+bool hasScholarship = true;
+int currentyear = 2026;
+int age = currentyear - birthYear;
